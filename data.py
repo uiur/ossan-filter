bfb2a05ab20222399289a0a983c65f4f0a6ec659
Kazato Sugimoto <uiureo@gmail.com>
2016-04-03 20:27:57 +0900

    Rename data dir

--- data.py
+++ data.py
@@ -17,16 +17,16 @@ def load_image(pattern):
     return normalize(tf.cast(image, tf.float32))
 
 def load_ossan():
-    image = load_image('resized_data/train/ossan/*.png')
+    image = load_image('data/train/ossan/*.png')
     return tf.tuple([image, tf.constant([1., 0.])])
 
 def load_other():
-    image = load_image('resized_data/train/other/*.png')
+    image = load_image('data/train/other/*.png')
     return tf.tuple([image, tf.constant([0., 1.])])
 
 def load_test(batch_size=300):
-    ossan = load_image('resized_data/test/ossan/*.png')
-    other = load_image('resized_data/test/other/*.png')
+    ossan = load_image('data/test/ossan/*.png')
+    other = load_image('data/test/other/*.png')
 
     return tf.train.batch_join([tf.tuple([ossan, tf.constant([1., 0.])]), tf.tuple([other, tf.constant([0., 1.])])], batch_size, shapes=[IMAGE_SHAPE, [2]])
 
